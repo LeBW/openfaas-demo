1c83130053553bd922f5e93b1cc3212a707cb528
LeBW <27091925@qq.com>
2021-12-26 13:55:21 +0800

    change build_args

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-faas-cli up --build-arg service_name=a -f a.yml
-faas-cli up --build-arg service_name=b -f b.yml
-faas-cli up --build-arg service_name=c -f c.yml
+faas-cli up -f a.yml
+faas-cli up -f b.yml
+faas-cli up -f c.yml
 
